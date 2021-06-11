683f521ccb73d9be9fb6edf6077c90df16a4e586
mayankthakur0007 <mayankthakur231@gmail.com>
2021-06-11 13:44:39 +0530

    Home screen changed

--- src/FirstLoadPage.jsx
+++ src/FirstLoadPage.jsx
@@ -1,36 +1,59 @@
 import "./FirstLoadPage.css";
-import { useEffect, useState } from "react";
 import Menu from "./Menu";
 import Intro from "./Intro";
 import LoadingScreen from "./LoadingScreen";
 import "./Animation.css";
+import { useEffect, useState } from "react";
+import WbSunnyRoundedIcon from "@material-ui/icons/WbSunnyRounded";
+import NightsStayRoundedIcon from "@material-ui/icons/NightsStayRounded";
 
 function FirstLoadPage() {
   const [setContentClass, addContentClass] = useState("App-content");
-  const [showContent, changeShowContent] = useState(false);
   useEffect(() => {
     setTimeout(() => {
       addContentClass("App-content slide");
     }, 4000);
     setTimeout(() => {
-      changeShowContent(true);
-    }, 5000);
-    setTimeout(() => {
-      document.querySelector(".loading").innerHTML = "";
       addContentClass("slide");
+      document.querySelector(".loading").innerHTML = "";
     }, 6000);
   }, []);
+
+  const [iconC, chanceIconC] = useState("white");
+  const [day, changeIcon] = useState(true);
+  const [mode, setMode] = useState("night");
+
+  const modeChange = () => {
+    if (mode === "day") {
+      setMode("night");
+      chanceIconC("white");
+      changeIcon(true);
+    } else {
+      setMode("day");
+      changeIcon(false);
+      chanceIconC("black");
+    }
+  };
   return (
     <div className="App">
       <div className="loading">
         <LoadingScreen></LoadingScreen>
       </div>
-      <div className={setContentClass}>
-        <div className="flexbox">
-          <div className="yellow">{showContent ? <Menu></Menu> : ""}</div>
-          <div className="black">{showContent ? <Intro></Intro> : ""}</div>
+        <div className={setContentClass}>
+          <div className={mode}>
+            <Menu iconC={iconC} />
+            <div className="mode">
+              {day ? (
+                <WbSunnyRoundedIcon onClick={modeChange}></WbSunnyRoundedIcon>
+              ) : (
+                <NightsStayRoundedIcon
+                  onClick={modeChange}
+                ></NightsStayRoundedIcon>
+              )}
+            </div>
+            <Intro />
+          </div>
         </div>
-      </div>
     </div>
   );
 }
