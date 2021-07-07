1a8fb2bfc5268eec3232fcef151e9ca893cdaf57
mayankthakur0007 <mayankthakur231@gmail.com>
2021-07-07 09:16:11 +0530

    spacing fix

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -10,22 +10,22 @@ const Contact = () => {
       <Fade right>
         <section className="socialMedia">
           <ul className="sci">
-            <li>
+            <li className="icons">
               <a href="https://www.facebook.com/mayankthakur007" target="_blank">
                 <i className="fa fa-facebook-official" aria-hidden="true"></i>
               </a>
             </li>
-            <li>
+            <li className="icons">
               <a href="https://www.instagram.com/mayank_thakur0810/" target="_blank">
                 <i className="fa fa-instagram" aria-hidden="true"></i>
               </a>
             </li>
-            <li>
+            <li className="icons">
               <a href="https://github.com/mayankthakur0007" target="_blank">
                 <i className="fa fa-git" aria-hidden="true"></i>
               </a>
             </li>
-            <li>
+            <li className="icons">
               <a href="https://www.linkedin.com/in/mayank-pratap-singh-31464962/" target="_blank">
                 <i className="fa fa-linkedin" aria-hidden="true"></i>
               </a>
